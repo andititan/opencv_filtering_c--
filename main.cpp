e06e4738d193b34567baa958084b141f0fd56903
andititan <pirinkovandi@gmail.com>
2022-12-14 16:33:58 +0200

    blob detection and more test pictures

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 using namespace cv;
 
 int main(){
-    Mat image = imread("../27307090.jpg");
+    Mat image = imread("../a3d2f76670f48030854736790e9dd1e4.jpg");
     if ( !image.data ){
         printf("No image data \n");
         return -1;
@@ -26,11 +26,40 @@ int main(){
     morphologyEx(full_mask, full_mask, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)));
     morphologyEx(full_mask, full_mask, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(3, 3)));
 
+    // Set up SimpleBlobDetector parameters.
+    cv::SimpleBlobDetector::Params params;
+
+    params.filterByInertia = false;
+    params.filterByConvexity = false;
+    params.filterByCircularity = false;
+    params.filterByColor = true;
+
+    // Change thresholds
+    params.minThreshold = 10;
+    params.maxThreshold = 255;
+    params.blobColor = 255;
+
+    // Filter by Area
+    params.filterByArea = true;
+    params.minArea = 0;
+    params.maxArea = 90000000;
+    params.minDistBetweenBlobs = 0.0f;
+
+    // Perform the detection
+    cv::Ptr<cv::SimpleBlobDetector> detector = 
+    cv::SimpleBlobDetector::create(params);
+    std::vector<cv::KeyPoint> keypoints;
+    detector->detect(full_mask, keypoints);
+
+    cv::Mat im_with_keypoints;
+    drawKeypoints(full_mask, keypoints, im_with_keypoints, cv::Scalar(0, 0, 255), 
+    cv::DrawMatchesFlags::DEFAULT);
+
     namedWindow("Original", WINDOW_AUTOSIZE );
     imshow("Original", image);
 
     namedWindow("New", WINDOW_AUTOSIZE );
-    imshow("New", full_mask);
+    imshow("New", im_with_keypoints);
 
     waitKey(0);
 
